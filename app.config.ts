c128327d3ceb3f8292e844d8a3b6c145cae65522
dark1zinn <edilsonjuininho154@gmail.com>
2025-07-08 02:11:39 -0300

    ⚙️ Add theming configuration

---
+++ app.config.ts
@@ -0,0 +1,9 @@
+export default defineAppConfig({
+    ui: {
+        colors: {
+            primary: 'orange-500',
+            secondary: 'teal-400',
+            neutral: 'zinc',
+        },
+    },
+});
