e8fc3bb8a6c0b6e0240aabe820508592f1ee7f9b
dark1zinn <edilsonjuininho154@gmail.com>
2025-08-23 22:22:46 -0300

    🔨 Added middleware for discord invite redirection

---
+++ app/pages/discord.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+definePageMeta({
+  middleware: 'discord'
+});
+</script>
+
+<template>
+  <section class="size-full h-full flex flex-col items-center justify-center">
+    You are being redirected to our Discord...
+  </section>
+</template>
